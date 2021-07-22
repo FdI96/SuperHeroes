ff375763b29cb69f74f4b31c680c0f62709fea8e
FdI96 <federicolopezce@gmail.com>
2021-07-22 13:01:35 -0300

    Add dropdown buttons to every hero to display when a click is done

--- src/components/showHeroes.js
+++ src/components/showHeroes.js
@@ -5,43 +5,73 @@ import { Card, CardSubtitle, CardTitle, CardText, Row, Col } from "reactstrap";
 const ShowHeroes = (props) => {
   return (
     <Row>
-      {props.heroesCollection.map((element) => {
-        return (
-          <Col>
-            <Card body color="success" className="card-height">
-              <CardTitle tag="h5">{element.name}</CardTitle>
-              <CardSubtitle tag="h6" className="mb-2">
-                Team:
-                {element.team === " "
-                  ? "This hero works alone"
-                  : element.team.map((e) => <div key="{e.name}">{e.name}</div>)}
-              </CardSubtitle>
-              <CardText>
-                Missions:
-                {element.mission.map((e) => (
-                  <div key="{e.code}">
-                    {e.description + " is a type: " + e.type}
+      <div class="accordion" id="accordionExample">
+        {props.heroesCollection.map((element) => {
+          return (
+            <Col>
+              <div class="accordion-item">
+                <h2 class="accordion-header" id={"heading-" + element.name}>
+                  <button
+                    class="accordion-button"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target={"#collapse-" + element.name}
+                    aria-expanded="true"
+                    aria-controls={"#collapse-" + element.name}
+                  >
+                    {element.name}
+                  </button>
+                </h2>
+                <div
+                  id={"collapse-" + element.name}
+                  class="accordion-collapse collapse show"
+                  aria-labelledby={"heading-" + element.name}
+                >
+                  <div class="accordion-body">
+                    <Card
+                      body
+                      color="success"
+                      className="card-height rounded shadow-lg"
+                    >
+                      <CardTitle tag="h5">{element.name}</CardTitle>
+                      <CardSubtitle tag="h6" className="mb-2">
+                        Team:
+                        {element.team === " "
+                          ? "This hero works alone"
+                          : element.team.map((e) => (
+                              <div key="{e.name}">{e.name}</div>
+                            ))}
+                      </CardSubtitle>
+                      <CardText>
+                        Missions:
+                        {element.mission.map((e) => (
+                          <div key="{e.code}">
+                            {e.description + " is a type: " + e.type}
+                          </div>
+                        ))}
+                      </CardText>
+                      <CardText>
+                        SuperPowers:
+                        {element.superpower.map((e) => (
+                          <div key="{e.code}">
+                            {e.description + " with level: " + e.level}
+                          </div>
+                        ))}
+                      </CardText>
+                      Age:
+                      <CardText>{element.year}</CardText>
+                      Planet:
+                      <CardText>
+                        {element.planet === null ? "Earth" : element.planet}
+                      </CardText>
+                    </Card>
                   </div>
-                ))}
-              </CardText>
-              <CardText>
-                SuperPowers:
-                {element.superpower.map((e) => (
-                  <div key="{e.code}">
-                    {e.description + " with level: " + e.level}
-                  </div>
-                ))}
-              </CardText>
-              Age:
-              <CardText>{element.year}</CardText>
-              Planet:
-              <CardText>
-                {element.planet === null ? "Earth" : element.planet}
-              </CardText>
-            </Card>
-          </Col>
-        );
-      })}
+                </div>
+              </div>
+            </Col>
+          );
+        })}
+      </div>
     </Row>
   );
 };
