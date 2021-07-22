9f685c7b04ba69c7e9ac494d596cbdafd632e11f
FdI96 <federicolopezce@gmail.com>
2021-07-21 23:46:06 -0300

    Add ShowHeroes fuctional component to display all the heroes from the jsonfile

--- src/App.js
+++ src/App.js
@@ -1,35 +1,8 @@
 import "./App.css";
-import {
-  Card,
-  CardText,
-  CardBody,
-  CardLink,
-  CardTitle,
-  CardSubtitle,
-} from "reactstrap";
+import ShowHeroes from "./components/showHeroes";
 
 function App() {
-  return (
-    <div>
-      <Card>
-        <CardBody>
-          <CardTitle tag="h5">Card title</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">
-            Card subtitle
-          </CardSubtitle>
-        </CardBody>
-        <img width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-        <CardBody>
-          <CardText>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </CardText>
-          <CardLink href="#">Card Link</CardLink>
-          <CardLink href="#">Another Link</CardLink>
-        </CardBody>
-      </Card>
-    </div>
-  );
+  return <ShowHeroes></ShowHeroes>;
 }
 
 export default App;
---
+++ src/components/showHeroes.js
@@ -0,0 +1,49 @@
+import React from "react";
+import heroes from "../model/heros.json";
+import { Card, CardSubtitle, CardTitle, CardText, Row, Col } from "reactstrap";
+
+const ShowHeroes = () => {
+  return (
+    <Row>
+      {heroes.map((element) => {
+        return (
+          <Col sm="12">
+            <Card body>
+              <CardTitle tag="h5">{element.name}</CardTitle>
+              <CardSubtitle tag="h6" className="mb-2 text-muted">
+                Team:
+                {element.team === []
+                  ? "This hero works alone"
+                  : element.team.map((e) => <div key="{e.name}">{e.name}</div>)}
+              </CardSubtitle>
+              <CardText>
+                Missions:
+                {element.mission.map((e) => (
+                  <div key="{e.code}">
+                    {e.description + " is a type: " + e.type}
+                  </div>
+                ))}
+              </CardText>
+              <CardText>
+                SuperPowers:
+                {element.superpower.map((e) => (
+                  <div key="{e.code}">
+                    {e.description + " with level: " + e.level}
+                  </div>
+                ))}
+              </CardText>
+              Age:
+              <CardText>{element.year}</CardText>
+              Planet:
+              <CardText>
+                {element.planet === null ? "Earth" : element.planet}
+              </CardText>
+            </Card>
+          </Col>
+        );
+      })}
+    </Row>
+  );
+};
+
+export default ShowHeroes;
